8351aa3df22b163e969c84f82e196c0c77064f23
SyazwanYacobUiTMJasin <muhdsyazwan5703@gmail.com>
2026-01-12 13:45:42 +0800

    add not found page with translation support

--- src/app/pages/not-found/not-found.component.ts
+++ src/app/pages/not-found/not-found.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-not-found',
-  imports: [],
+  imports: [RouterLink, TranslatePipe],
   templateUrl: './not-found.component.html',
   styleUrl: './not-found.component.scss'
 })
